b3af21ea18a9718f93be8d5aa95fd53cc2131f60
Joa-e10 <joaquin.ferreyra.e@gmail.com>
2026-05-09 09:42:15 -0300

    Cambios en escena
    Se agregaron ítems de prueba en la escena, se actualizaron los prefabs y se organizaron los archivos de la carpeta "assets".

--- Assets/Scripts/ItemData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu (fileName = "New Item Data",menuName = "ItemData")]
-public class ItemData : ScriptableObject
-{
-    [SerializeField] private string _itemName;
-    [SerializeField] private int _itemAmount;
-    [SerializeField] private string _itemType;
-}
